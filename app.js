117fd563424be81f7ab7ca9d5dbe954b7122c5b5
devinsg <duyanh2005@gmail.com>
2020-09-29 19:50:23 +0700

    - make refractor codes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var hana = require('@sap/hana-client');
 
-const queryData1 = function() {  
+const queryData = function() {  
   var conn = hana.createConnection();
   var conn_params = {
     serverNode  : 'localhost:39017',
@@ -26,31 +26,4 @@ const queryData1 = function() {
   return true;
 }
 
-const queryData2 = function() {  
-    var conn = hana.createConnection();
-    var conn_params = {
-      serverNode  : 'localhost:39017',
-      uid         : 'SYSTEM',
-      pwd         : 'HXEHana1'
-    };
-  
-    return conn.connect(conn_params, function(err) {
-      var sql2 = 'SELECT Name, Description FROM Products WHERE id = ?'
-      if (err) {
-        console.log('ERR:', err);
-        throw err;
-      }
-  
-      conn.exec(sql2, [301], function (err, result) {
-        if (err) throw err;
-        console.log('Name: ', result[0].Name, ', Description: ', result[0].Description);
-        // output --> Name: Tee Shirt, Description: V-neck
-        conn.disconnect();
-      })
-    });
-    
-    return true;
-  }
-
-queryData1()
-//queryData2()
+queryData()
--- common/dbContext.js
+++ common/dbContext.js
@@ -1,16 +1,13 @@
 var hana = require('@sap/hana-client');
 
 const queryData = function() {
-  /*
   var conn = hana.createConnection();
   var conn_params = {
     serverNode  : 'localhost:39017',
     uid         : 'SYSTEM',
     pwd         : 'HXEHana1'
   };
-  */
- 
-  /*
+
   return conn.connect(conn_params, function(err) {
     var sql1 = 'SELECT TOP 10 TABLE_NAME, RECORD_COUNT, TABLE_SIZE FROM M_TABLES ORDER BY TABLE_SIZE DESC'
     var sql2 = 'SELECT Name, Description FROM Products WHERE id = ?'
@@ -26,7 +23,6 @@ const queryData = function() {
       conn.disconnect();
     })
   });
-  */
   return true;
 }
 
